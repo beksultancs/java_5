97e05d0b189cd1f59451a93a72c153d974215173
Aizhan Nurmatova <AizhanFromPekasoft>
2022-04-08 12:14:10 +0600

    change

--- src/peaksoft/Main.java
+++ src/peaksoft/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello java 5");
-        System.out.println("commit");
         SomeClassByJ.myFeatures();
         System.out.println("Barchynai's jdk doesn't work");
+
+        System.out.println("commit");
     }
 }
